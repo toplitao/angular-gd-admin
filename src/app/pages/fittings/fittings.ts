9c24ef1bf72742a457748e7c200e89fcd984fb3a
litao <724062586@qq.com>
2017-03-22 23:10:22 +0800

    修改

---
+++ src/app/pages/fittings/fittings.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService, AuthGuard } from '../../providers';
+@Component({
+    selector: 'fittings',
+    styleUrls: ['fittings.scss'],
+    templateUrl: 'fittings.html'
+})
+export class FittingsComponent {
+
+}
