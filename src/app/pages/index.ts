6cc04b34a11a4725fb1a9ee4370e81809676cb30
ylt94 <857425891@qq.com>
2017-03-26 02:00:04 +0800

    xiugai

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -3,7 +3,7 @@ export * from './login/login';
 export * from './user/user.module';
 export * from './goods/goods.component'
 export * from './order/order.module';
-export * from './fitting/fitting.module';
+//export * from './fitting/fitting.module';
 export * from './information/information.module';
 export * from './shared/shared.module';
 export * from './interactive/interactive.module';
